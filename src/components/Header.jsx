de7ef31db7d1aab50018c241600696388c21a12a
John A. Samuel <johnayobami77@proton.me>
2025-01-11 23:32:15 +0100

    More updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,26 @@
-import { HiOutlineMenuAlt3, HiX } from "react-icons/hi";
+import { HiMenuAlt2, HiX } from "react-icons/hi";
 import { useState } from "react";
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
-        <header className="fixed w-full flex items-center justify-between bg-transparent backdrop-blur-xl px-6 md:px-28 py-5 z-30">
+        <header className="fixed w-full flex items-center justify-between bg-transparent backdrop-blur-xl px-5 md:px-28 py-7 md:py-8 z-30">
             <div>
                 <img src="/forcythe_logo.svg" className="w-28" alt="Forcythe Logo" />
             </div>
             <nav
-                className={`absolute md:static top-14 right-10 border-r border-r-white max-w-sm w-full rounded-3xl md:rounded-none md:p-0 md:w-auto md:h-auto bg-bg md:bg-transparent text-white md:text-black rounded-xl md:rounded-none flex flex-col md:flex-row items-center justify-center space-y-6 md:space-y-0 md:space-x-7 transition-transform duration-300 ${isMenuOpen ? "block" : "hidden"
+                className={`absolute md:static top-28 right-8 w-2/4 px-8 py-10 rounded-2xl md:rounded-none md:p-0 md:w-auto md:h-auto bg-gradient-to-b from-[#061626] to-black md:bg-transparent text-white md:text-black rounded-xl md:rounded-none flex flex-col md:flex-row items-center justify-center space-y-6 md:space-y-0 md:space-x-7 transition-transform duration-300 ${isMenuOpen ? "block" : "hidden"
                     } md:translate-x-0 z-20`}
             >
-                <ul className="flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-7 text-white">
+                <ul className="flex  flex-col md:flex-row items-start md:items-center mr-auto space-y-6 md:space-y-0 md:space-x-7 text-white">
                     <li><a href="#">About</a></li>
                     <li><a href="#">Services</a></li>
                     <li><a href="#">Portfolio</a></li>
                     <li><a href="#">Studio</a></li>
                     <li><a href="#">Foundation</a></li>
+                    <li className="block md:hidden"><a href="#">Career</a></li>
+                    <li className="block md:hidden"><a href="#">Blog</a></li>
                 </ul>
             </nav>
                 <a href="#" className="hidden md:block relative group">
@@ -26,10 +28,10 @@ const Header = () => {
                     <span className="absolute w-full h-full top-1.5 right-1.5 z-0 rounded-full border-[1px] border-dashed group-hover:border-[#064386] transition-all duration-300 ease"></span>
                 </a>
             <button
-                className="text-white text-2xl md:hidden z-30"
+                className="text-white text-2xl md:hidden z-30 bg-white bg-opacity-10 rounded-lg p-2"
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
             >
-                {isMenuOpen ? <HiX /> : <HiOutlineMenuAlt3 />}
+                {isMenuOpen ? <HiX /> : <HiMenuAlt2 />}
             </button>
         </header>
     );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,13 +2,13 @@ import { FaCaretRight } from "react-icons/fa6";
 
 const Hero = () => {
     return (
-        <div className="px-28 relative">
+        <div className="px-6 lg:px-16 xl:px-28 relative">
             <div>
-                <img src="/header-background.svg" alt="" className="absolute w-11/12 left-1/3 -translate-x-1/3" />
+                <img src="/header-background.svg" alt="" className="absolute object-cover w-full h-full md:w-11/12 left-1/3 -translate-x-1/3" />
             </div>
-            <div className="flex flex-col items-start justify-center w-full bg-white bg-opacity-10 rounded-[2rem] sm:rounded-[3rem] p-5 py-8 md:p-10 lg:p-14 my-10 space-y-6 z-10">
-                <h1 className="text-3xl md:text-4xl lg:text-6xl xl:text-8xl w-11/12">We build <span className="text-secondary">products</span> that shape a better future</h1>
-                <p className="text-base lg:text-lg text-gray-400 w-3/5">We’re the architects of digital excellence across industries. We redefine business with cutting-edge digital strategies that invokes sector-wide transformation.</p>
+            <div className="flex flex-col items-start justify-center w-full bg-white bg-opacity-10 rounded-[2rem] sm:rounded-[3rem] px-6 py-10 md:p-10 lg:p-14 my-10 space-y-6 z-10">
+                <h1 className="text-6xl xl:text-8xl w-full md:w-11/12">We build <span className="text-secondary">products</span> that shape a better future</h1>
+                <p className="text-base lg:text-lg text-gray-400 w-full md:w-3/5">We’re the architects of digital excellence across industries. We redefine business with cutting-edge digital strategies that invokes sector-wide transformation.</p>
                 <a href="#" className="relative group">
                     <button className="bg-white px-5 py-3 rounded-full group-hover:bg-[#064386] text-bg font-semibold group-hover:text-white transition-all duration-300 ease">Book a call <FaCaretRight className="inline-block ml-0.5 text-lg -translate-y-0.5 group-hover:hidden" /></button>
                     <span className="absolute w-full h-full top-1.5 right-1.5 z-0 rounded-full border-[1px] border-dashed group-hover:border-[#064386] transition-all duration-300 ease"></span>
