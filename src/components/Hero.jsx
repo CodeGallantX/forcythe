eef2747d40e8f4a2eede3cf5465b6a232ee55307
John A. Samuel <johnayobami77@proton.me>
2025-01-11 04:59:34 +0100

    Updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
 const Hero = () => {
     return (
         <div>
-            <div className="flex flex-col items-start space-y-5 px-14 bg-board rounded-[4rem] py-16">
-                <h1 className="text-6xl">We build <span className="text-secondary">products</span> that shape a better future</h1>
-                <p className="text-xl text-gray-400">We’re the architects of digital excellence across industries. We redefine business with cutting-edge digital strategies that invokes sector-wide transformation.</p>
+            <div className="flex flex-col items-start space-y-5 px-10 bg-board rounded-[4rem] py-10">
+                <h1 className="text-7xl w-4/5">We build <span className="text-secondary">products</span> that shape a better future</h1>
+                <p className="text-lg text-gray-300 max-w-2xl">We’re the architects of digital excellence across industries. We redefine business with cutting-edge digital strategies that invokes sector-wide transformation.</p>
                 <a href="#" className="bg-white px-4 py-3 rounded-full text-bg">Book a call</a>
             </div>
         </div>
