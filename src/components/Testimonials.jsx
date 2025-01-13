6212066bcad1cbaaa1ec665b5d46ee9fc4364f5a
John A. Samuel <johnayobami77@proton.me>
2025-01-13 04:08:02 +0100

    Updates

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 
 const Testimonials = () => {
     const companies = [
-        { name: "Starks Associates", logo: "/starks.svg", src:"/john.svg", testimonial: "Partnering with Forcythe was like finding a hidden gem. Their genuine interest in our success was palpable, and the continuous support post-launch has been a testament to their commitment. They’ve become more than a service provider; they’re a trusted ally.", author: "John Doe, CEO" },
+        { name: "Starks Associates", logo: "/starks.svg", src:"/john.svg", testimonial: "Partnering with Forcythe was like finding a hidden gem. Their genuine interest in our success was palpable, and the continuous support post-launch has been a testament to their commitment. They’ve become more than a service provider; they’re a trusted ally.", author: "John, CEO" },
         { name: "ExecutivePros", logo: "/ExecutivePros_Logo.svg", src:"/testimony.svg", testimonial: "The team understood the assignment and delivered very well. One of the things that stood them out was how they took our concepts and turned it into visually appealing designs that caught the eyes of our clients and made increased web engagements. Kudos!", author: "Testimony, Co-founder" },
         { name: "Stac AI", logo: "/stacai.svg", src:"/edwin.svg", testimonial: "Forcythe is seriously amazing when it comes to coming up with new ideas. They took our rough ideas and turned them into something incredible online. Their team’s commitment to our vision was evident every step of the way.", author: "Edwin, Former CTO" },
         { name: "Iwaria", logo: "/iwaria.svg", src:"/iwaria-founder.svg", testimonial: "The moment we engaged Forcythe, it was clear they were in a league of their own. Their strategic approach to our project not only enhanced our online platform but also enriched our brand’s story, captivating our audience like never before.", author: "Iwaria, Founder" },
-        { name: "Beaupreneur", logo: "/Beaupreneur.svg", src:"/christina.svg", testimonial: "I’m beyond impressed with the dedication Forcythe showed. They didn’t just deliver—they educated us, involving us in the creative process. The result? A website that truly feels like ours and speaks directly to our customers’ hearts.", author: "Christiana, Founder" },
+        { name: "Beaupreneur", logo: "/Beaupreneur.svg", src:"/christiana.svg", testimonial: "I’m beyond impressed with the dedication Forcythe showed. They didn’t just deliver—they educated us, involving us in the creative process. The result? A website that truly feels like ours and speaks directly to our customers’ hearts.", author: "Christiana, Founder" },
     ];
 
     const [activeTab, setActiveTab] = useState(0);
@@ -65,7 +65,7 @@ const Testimonials = () => {
                 <div className="sm:w-2/5 relative">
                     <div className="absolute inset-0 bg-[#0c2745] opacity-10 animate-pulse"></div>
                     <img
-                        src="christina.svg"
+                        src={companies[activeTab].src}
                         alt="Author"
                         className="relative w-full h-64 sm:h-auto object-cover rounded-xl"
                     />
