408f99fc0ee012c3605240254f2248c8b5aa09d8
John A. Samuel <johnayobami77@proton.me>
2025-01-13 04:28:01 +0100

    Updates

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -3,10 +3,10 @@ import { useState } from "react";
 const Testimonials = () => {
     const companies = [
         { name: "Starks", logo: "/starks.svg", src:"/john.svg", testimonial: "Partnering with Forcythe was like finding a hidden gem. Their genuine interest in our success was palpable, and the continuous support post-launch has been a testament to their commitment. They’ve become more than a service provider; they’re a trusted ally.", author: "John, CEO" },
-        { name: "ExecutivePros", logo: "/ExecutivePros_Logo.svg", src:"/testimony.svg", testimonial: "The team understood the assignment and delivered very well. One of the things that stood them out was how they took our concepts and turned it into visually appealing designs that caught the eyes of our clients and made increased web engagements. Kudos!", author: "Testimony, Co-founder" },
+        { name: "ExecutivePros", logo: "/ExecutivePros_Logo.svg", src:"/executive-pro-ceo.svg", testimonial: "The team understood the assignment and delivered very well. One of the things that stood them out was how they took our concepts and turned it into visually appealing designs that caught the eyes of our clients and made increased web engagements. Kudos!", author: "Testimony, Co-founder" },
         { name: "Stac AI", logo: "/stacai.svg", src:"/edwin.svg", testimonial: "Forcythe is seriously amazing when it comes to coming up with new ideas. They took our rough ideas and turned them into something incredible online. Their team’s commitment to our vision was evident every step of the way.", author: "Edwin, Former CTO" },
         { name: "Iwaria", logo: "/iwaria.svg", src:"/iwaria-founder.svg", testimonial: "The moment we engaged Forcythe, it was clear they were in a league of their own. Their strategic approach to our project not only enhanced our online platform but also enriched our brand’s story, captivating our audience like never before.", author: "Iwaria, Founder" },
-        { name: "Beaupreneur", logo: "/Beaupreneur.svg", src:"/christiana.svg", testimonial: "I’m beyond impressed with the dedication Forcythe showed. They didn’t just deliver—they educated us, involving us in the creative process. The result? A website that truly feels like ours and speaks directly to our customers’ hearts.", author: "Christiana, Founder" },
+        { name: "Beaupreneur", logo: "/Beaupreneur.svg", src:"/christina.svg", testimonial: "I’m beyond impressed with the dedication Forcythe showed. They didn’t just deliver—they educated us, involving us in the creative process. The result? A website that truly feels like ours and speaks directly to our customers’ hearts.", author: "Christiana, Founder" },
     ];
 
     const [activeTab, setActiveTab] = useState(0);
@@ -34,20 +34,20 @@ const Testimonials = () => {
                                             ? "rounded-r-full"
                                             : "rounded-none"
                                     }`
-                                    : "text-gray-800"
+                                    : "text-bg"
                             }`}
                         >
                             <img
                                 src={company.logo}
                                 alt={company.name}
-                                className={`w-10 h-10  object-contain mr-2 ${
+                                className={`w-10 h-10  object-contain ${
                                     index === 0
-                                        ? "sm:w-12 sm:h-auto"
-                                        : "sm:w-36 sm:h-auto"
+                                        ? "sm:w-10 sm:h-full"
+                                        : "sm:max-w-32 sm:min-w-20 sm:w-full sm:h-full"
                                 }`}
                             />
                             {index === 0 && company.name && (
-                                <span className="text-base font-semibold">{company.name}</span>
+                                <span className="text-base font-semibold"> {company.name}</span>
                             )}
                         </button>
                     ))}
