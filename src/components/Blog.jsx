384bc8b647312ed5fee572444d967494e40d3e5b
John A. Samuel <johnayobami77@proton.me>
2025-01-13 05:50:14 +0100

    Updates

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -34,21 +34,21 @@ const Blog = () => {
                     <span className="absolute w-full h-full top-1.5 right-1.5 z-0 rounded-full border-[1px] border-dashed group-hover:border-[#064386] transition-all duration-300 ease"></span>
                 </a>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 mt-10 items-start">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-12 mt-10 items-start">
                 {posts.map((post, index) => (
-                    <a key={index} href="#" className="w-full flex flex-col items-start justify-center rounded-2xl group  hover:border-b-2 hover:border-b-[#b3d0f2] hover:border-x hover:border-x-primary transition-all duration-300 ease-in-out">
-                        <div className="bg-[#070e21] max-w-2xl h-auto">
-                            <img src={post.img} alt={post.title} loading="lazy" className="w-full object-cover rounded-xl" />
+                    <a key={index} href="#" className="w-full flex flex-col items-start justify-center rounded-2xl group  hover:ring-1 hover:ring-[#064386] transition-all duration-500 ease-in-out delay-100">
+                        <div className="bg-[#070e21] max-w-2xl h-full">
+                            <img src={post.img} alt={post.title} loading="lazy" className="w-full h-52 object-cover rounded-xl" />
                         </div>
                         <div className="flex flex-col items-start justify-center py-6 group-hover:px-3 transition-all duration-300 ease-in-out">
-                            <div className="p-3 border-l border-l-white transition-all duration-300 ease-in-out">
+                            <div className="px-3 py-0 border-l border-l-white transition-all duration-300 ease-in-out">
                                 <h5 className="font-medium text-xl">{post.category}</h5>
                                 <div className="flex flex-row items-center justify-start space-x-2 text-gray-400">
                                     <span>{post.author}</span>
-                                    <span className="w-3 h-3 rounded-full bg-white"></span>
+                                    <span className="w-2 h-2 rounded-full bg-white"></span>
                                     <span>{post.date}</span>
                                 </div>
-                                <h3 className="text-3xl font-semibold mt-3">{post.title}</h3>
+                                <h3 className="text-2xl font-semibold mt-3">{post.title}</h3>
                             </div>
                         </div>
                     </a>
