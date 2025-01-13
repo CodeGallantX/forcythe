77cbf7ea3e49ce5478b895008d857989fd1f4e34
John A. Samuel <johnayobami77@proton.me>
2025-01-13 04:47:58 +0100

    Updates

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -12,8 +12,8 @@ const Blog = () => {
         }
     ]
     return (
-        <div className="py-10">
-            <div className="px-28 flex flex-row items-center justify-between">
+        <div className="py-10 px-6 lg:px-16 xl:px-28">
+            <div className="flex flex-row items-center justify-between">
                 <h1 className="text-4xl">Read our articles, news and product blog</h1>
                 <a href="#" className="relative group">
                     <button className="bg-white px-5 py-3 rounded-full group-hover:bg-[#064386] text-bg font-semibold group-hover:text-white transition-all duration-300 ease">Visit Blog <FaCaretRight className="inline-block ml-0.5 text-lg -translate-y-0.5 group-hover:hidden" /></button>
@@ -21,21 +21,26 @@ const Blog = () => {
                 </a>
             </div>
             <div>
-                {posts.map((post, index) => (
-
-                    <div key={index}>
-                        <a href="#">
-                            <div className="bg-[#070e21]">
-                                <img src={post.img} alt={post.title} />
-                            </div>
-                            <div>
-                                <h3>{post.title}</h3>
-                                <span>{post.author} * {post.date}</span>
-                            </div>
-                        </a>
-
-                    </div>
-                ))
+                <div>
+                    {posts.map((post, index) => (
+                        <div key={index} className="rounded-2xl">
+                            <a href="#">
+                                <div className="bg-[#070e21] max-w-md h-auto">
+                                    <img src={post.img} alt={post.title} loading="lazy" className="w-full object-cover" />
+                                </div>
+                                <div>
+                                    <h5>{post.category}</h5>
+                                    <div>
+                                        <span>{post.author}</span>
+                                        <span className="w-10 h-10 rounded-full bg-white"></span>
+                                        <span>{post.date}</span>
+                                    </div>
+                                    <h3>{post.title}</h3>
+                                </div>
+                            </a>
+                        </div>
+                    ))}
+                </div>
 
             </div>
         </div>
