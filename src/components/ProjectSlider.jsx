ea1c2120692b307c8dbe69e7f6c7004d341d469a
John A. Samuel <johnayobami77@proton.me>
2025-01-13 07:24:47 +0100

    Updates

--- src/components/ProjectSlider.jsx
+++ src/components/ProjectSlider.jsx
@@ -1,4 +1,7 @@
-import { useEffect, useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/autoplay";
+import { Autoplay } from "swiper";
 
 const ProjectSlider = () => {
   const images = [
@@ -6,76 +9,66 @@ const ProjectSlider = () => {
     "/activity.svg",
     "/africaFund.svg",
     "/starks_project.svg",
-    "/phone.svg",
     "/exec-pro.svg",
+    "/phone.svg",
   ];
 
-  const sliderTopRef = useRef(null);
-  const sliderBottomRef = useRef(null);
-
-  useEffect(() => {
-    const topAnimation = sliderTopRef.current;
-    const bottomAnimation = sliderBottomRef.current;
-
-    if (topAnimation && bottomAnimation) {
-      topAnimation.style.animationPlayState = "running";
-      bottomAnimation.style.animationPlayState = "running";
-    }
-
-    return () => {
-      if (topAnimation && bottomAnimation) {
-        topAnimation.style.animationPlayState = "paused";
-        bottomAnimation.style.animationPlayState = "paused";
-      }
-    };
-  }, []);
-
   return (
-    <div className="py-0 bg-bg">
-      <div className="min-h-[60px] text-center mb-8">
-        <p className="text-[2rem] sm:text-[2.2rem] lg:text-[2.6rem] leading-[2.5rem] lg:leading-[3rem] max-w-[90%] mx-auto">
-          <span className="text-gray-100">Success in </span>
-          <span className="text-[#60a6e7]">Motion</span> – Our clients’ journey
-        </p>
-      </div>
+    <div className="py-10 bg-gradient-to-b from-transparent to-bg">
+       <div className="min-h-[60px] text-center mb-8">
+         <p className="text-[2rem] sm:text-[2.2rem] lg:text-[2.6rem] leading-[2.5rem] lg:leading-[3rem] max-w-[90%] mx-auto">
+           <span className="text-gray-100">Success in </span>
+           <span className="text-[#60a6e7]">Motion</span> – Our clients’ journey
+         </p>
+       </div>
 
-      <div className="relative overflow-hidden">
-        <div
-          ref={sliderTopRef}
-          className="flex flex-row items-center justify-center space-x-20 animate-scroll-left whitespace-nowrap"
-        >
-          {[...images, ...images].map((src, index) => (
-            <div
-              key={`top-${index}`}
-              className="inline-block w-full h-auto mx-2"
-            >
-              <img
-                alt={`project-image-top-${index}`}
-                className="w-full h-auto object-cover"
-                src={src}
-              />
-            </div>
-          ))}
-        </div>
+       <Swiper
+        spaceBetween={30}
+        slidesPerView="auto"
+        loop={true}
+        autoplay={{
+          delay: 0,
+          disableOnInteraction: false,
+          reverseDirection: false, 
+        }}
+        speed={6000} 
+        modules={[Autoplay]}
+        className="w-full h-auto"
+      >
+        {[...images, ...images].map((src, index) => (
+          <SwiperSlide key={`top-${index}`} className="w-full">
+            <img
+              alt={`project-image-top-${index}`}
+              src={src}
+              className="w-full h-auto object-cover"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
-        <div
-          ref={sliderBottomRef}
-          className="animate-scroll-right whitespace-nowrap mt-6"
-        >
-          {[...images, ...images].map((src, index) => (
-            <div
-              key={`bottom-${index}`}
-              className="inline-block w-[150px] lg:w-[250px] mx-2"
-            >
-              <img
-                alt={`project-image-bottom-${index}`}
-                className="w-full h-auto object-cover"
-                src={src}
-              />
-            </div>
-          ))}
-        </div>
-      </div>
+    {/*   <Swiper
+    //     spaceBetween={30}
+    //     slidesPerView="auto"
+    //     loop={true}
+    //     autoplay={{
+    //       delay: 0,
+    //       disableOnInteraction: false,
+    //       reverseDirection: true,
+    //     }}
+    //     speed={6000} 
+    //     modules={[Autoplay]}
+    //     className="w-full h-auto mt-8"
+    //   >
+    //     {[...images, ...images].map((src, index) => (
+    //       <SwiperSlide key={`bottom-${index}`} className="w-full">
+    //         <img
+    //           alt={`project-image-bottom-${index}`}
+    //           src={src}
+    //           className="w-full h-auto object-cover"
+    //         />
+    //       </SwiperSlide>
+    //     ))}
+    //   </Swiper> */}
     </div>
   );
 };
